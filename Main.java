72d87056a466349260f8618444f5890bf9f39e9e
joannac112 <joannac112@nycstudents.net>
2024-11-18 14:33:01 +0000

    new

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+    }
+}
